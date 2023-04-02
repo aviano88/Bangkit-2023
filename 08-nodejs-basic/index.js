ac799a204c7195e522ce68701f588fe0ab1f95ae
aviano88 <67731935+aviano88@users.noreply.github.com>
2023-04-02 18:37:51 +0700

    add moment and lodash module

--- 08-nodejs-basic/index.js
+++ 08-nodejs-basic/index.js
@@ -3,3 +3,12 @@ const message = (name) => {
  }
   
  message('JavaScript');
+
+ const moment = require('moment');
+ 
+const date = moment().format("MMM Do YY");
+console.log(date);
+
+const _ = require('lodash');
+const myOddEvenArray = _.partition([1,2,3,4,5,6,9,10], n => n % 2);
+console.log(myOddEvenArray);
