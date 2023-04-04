a718d534deed94ee69a486e7cf2fd62ec236891a
aviano88 <67731935+aviano88@users.noreply.github.com>
2023-04-05 03:04:24 +0700

    ada routes.js for easier methor/verb request

--- 13-Javascript-Project/hapi-web-server/server.js
+++ 13-Javascript-Project/hapi-web-server/server.js
@@ -1,12 +1,15 @@
 console.log('Halo, kita akan membuat server menggunakan Hapi');
 
 const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const init = async () => {
     const server = Hapi.server({
         port: 5000,
         host: 'localhost',
     });
+
+    server.route(routes);
 
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
