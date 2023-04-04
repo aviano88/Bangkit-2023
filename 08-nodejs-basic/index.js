442a97039bd0111642f6f04c4aa5bf3632e03710
aviano88 <67731935+aviano88@users.noreply.github.com>
2023-04-04 17:44:15 +0700

    add file read

--- 08-nodejs-basic/index.js
+++ 08-nodejs-basic/index.js
@@ -12,3 +12,28 @@ console.log(date);
 const _ = require('lodash');
 const myOddEvenArray = _.partition([1,2,3,4,5,6,9,10], n => n % 2);
 console.log(myOddEvenArray);
+
+const { EventEmitter } = require('events');
+
+const birthdayEventListener = (name) => {
+  console.log(`Happy birthday ${name}!`);
+}
+
+const myEmitter = new EventEmitter();
+
+myEmitter.on('birthday', birthdayEventListener);
+
+myEmitter.emit('birthday', 'Dimas');
+
+const fs = require('fs');
+const { resolve } = require('path');
+
+const fileReadCallback = (error, data) => {
+    if (error) {
+        console.log('Gagal membaca file');
+        return;
+    }
+    console.log(data);
+};
+
+fs.readFileSync(resolve(__dirname, 'notes.txt'), 'utf-8', fileReadCallback);
