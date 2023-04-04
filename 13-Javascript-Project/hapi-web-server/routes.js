850182c1e02a06aface6c75c63e424623a5bb93f
aviano88 <67731935+aviano88@users.noreply.github.com>
2023-04-05 03:24:21 +0700

    add query parameters in routes.js

--- 13-Javascript-Project/hapi-web-server/routes.js
+++ 13-Javascript-Project/hapi-web-server/routes.js
@@ -28,6 +28,19 @@ const routes = [
         },
     },
     {
+        method: 'GET',
+        path: '/hello/{name?}',
+        handler: (request, h) => {
+            const { name = "stranger" } = request.params;
+            const { lang } = request.query;
+
+            if (lang === 'id') {
+                return `Hai, ${name}!`;
+            }
+            return `Hello, ${name}!`;
+        },
+    },
+    {
         method: '*',
         path: '/{any*}',
         handler: (request, h) => {
