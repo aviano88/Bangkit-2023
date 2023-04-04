65cb0dcbd347b52b3739f9f8f2fa4ad17bdb3d7b
aviano88 <67731935+aviano88@users.noreply.github.com>
2023-04-05 02:36:30 +0700

    creating new a new web server using hapi framework

---
+++ 13-Javascript-Project/hapi-web-server/server.js
@@ -0,0 +1,15 @@
+console.log('Halo, kita akan membuat server menggunakan Hapi');
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+    const server = Hapi.server({
+        port: 5000,
+        host: 'localhost',
+    });
+
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+}
+
+init();
